e4ad28c44bad5e3d15e42001e15f368281a51865
Sumit Dubey <sumit.dubey20317@gmail.com>
2025-05-27 20:26:45 +0530

    Initial Commit

--- RivoraWebApp/urls.py
+++ RivoraWebApp/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('Administration/', admin.site.urls),
     path('', include('Base.urls')),
     path('support', include('contact.urls')),
 ]
